83e3aaf5c52111b28da4bd2d554e97e0930ea03c
envsh <yatsen21@outlook.com>
2024-10-25 10:12:50 +0800

    .narinfo struct and simple fetch

--- src/main.v
+++ src/main.v
@@ -12,13 +12,26 @@ const hosturl = "https://mirrors.ustc.edu.cn"
 // const hosturl = "https://mirrors.tuna.tsinghua.edu.cn"
 // const chanurl = hosturl+"/nix-channels"
 // const pkgsurl = chanurl+"/nixpkgs-24.05-darwin"
+// const pkgsurl = chanurl+"/nixpkgs-unstable"
+// how get this url by store path???
+// nar pkg url: https://mirrors.ustc.edu.cn/nix-channels/store/nar/1b46dbba8m1dax0baqcnfwqvxah9h93hj638kf1l3jv0i2yqw123.nar.xz
+// https://nix.dev/manual/nix/2.24/store/store-path#:~:text=To%20make%20store%20objects%20accessible%20to%20operating%20system,Base32%20%2820%20arbitrary%20bytes%20become%2032%20ASCII%20characters%29
+// nix nar
+// narinfo
+// https://mirrors.ustc.edu.cn/nix-channels/store/w5v7c0ikm6mzf17f3zyjvf0cn8gcammm.narinfo
 
 struct Nixbase {
 	pub mut:
-	hosturl string	
+	hosturl string
+	metadir string
+	packdir string
+	tempdir string
+
+	stpath string
 }
 pub fn (me Nixbase) chanurl() string { return me.hosturl + "/nix-channels/"}
-pub fn (me Nixbase) pkgurl() string { return me.chanurl() + "nixpkgs-24.05-darwin/" }
+// pub fn (me Nixbase) pkgurl() string { return me.chanurl() + "nixpkgs-24.05-darwin/" }
+pub fn (me Nixbase) pkgurl() string { return me.chanurl() + "nixpkgs-unstable/" }
 pub fn (me Nixbase) storeurl() string { return me.chanurl() + "store/" }
 // 这个文件是什么格式？和 jq 相关，怎么解？
 // https://github.com/NixOS/nixpkgs/blob/63c4c8d6d77f80f6bee07672bdcfd8d6180fdd92/pkgs/top-level/make-tarball.nix#L54
@@ -27,6 +40,13 @@ pub fn (me Nixbase) pkgjson_url() string { return me.pkgurl() + "packages.json.b
 pub fn (me Nixbase) store_path_url() string { return me.pkgurl() + "store-paths.xz" }
 // 这个文件对应哪个目录？~/.nix-defexpr/channels/nixpkgs/pkgs/
 pub fn (me Nixbase) exprurl() string { return me.pkgurl() + "nixexprs.tar.xz" }
+// /nix/store/w5v7c0ikm6mzf17f3zyjvf0cn8gcammm-xxx =>
+// host///mirrors.ustc.edu.cn/nix-channels/store/w5v7c0ikm6mzf17f3zyjvf0cn8gcammm.narinfo
+pub fn (me Nixbase) store_path_tonarurl(stpath string) string {
+	hsval, pkg, ver := parse_nixstore_line(stpath)
+	return me.chanurl() + "store/${hsval}.narinfo"
+}
+pub fn (mut me Nixbase) set_stpath(stpath string) { me.stpath = stpath }
 // 在store/目录下的包名叫什么？？？
 // eg: nix copy -vvv /nix/store/7wpfn219p67x4i00ll2widi9bm2ysa82-pstree-2.39 --to tmpdir/  --impure --no-use-registries --no-update-lock-file --no-write-lock-file --no-recursive --refresh --repair
 // 如何得到包的大小？？？需要看nix copy怎么实现的！！！
@@ -34,6 +54,59 @@ pub fn (me Nixbase) exprurl() string { return me.pkgurl() + "nixexprs.tar.xz" }
 // 查找依赖包
 // copy下来的文件不能修改的问题，需要root？
 
+pub fn (mut me Nixbase) sync_meta() {
+	
+}
+
+pub fn (mut me Nixbase) fetch_narinfo(stpath string) Narinfo {
+	mut ch := curlv.new()
+	ch.url(me.store_path_tonarurl(stpath))
+	res := ch.get() or { panic(err) }
+	vcp.info(res.stcode, res.data.len, stpath)
+
+	return parse_narinfo(res.data)
+}
+
+// key: value format
+fn parse_narinfo(scc string) Narinfo {
+	mut narval := Narinfo{}
+	lines := scc.split_into_lines()
+	for line in lines {
+		fields := line.split(": ")
+		match fields[0] {
+			"StorePath" {narval.store_path=fields[1]}
+			"URL" {narval.relative_url=fields[1]}
+			"Compression" {narval.compression=fields[1]}
+			"FileHash" {narval.file_hash=fields[1]}
+			"FileSize" {narval.file_size=fields[1].i64()}
+			"NarHash" {narval.nar_hash=fields[1]}
+			"NarSize" {narval.nar_size=fields[1].i64()}
+			"References" {narval.references=fields[1].split(" ")}
+			"Deriver" {narval.deriver=fields[1]}
+			"Sig" {narval.signt=fields[1]}
+			else{vcp.info("notcap", fields.str())}
+		}
+	}
+	return narval
+}
+
+// eg.: https://mirrors.ustc.edu.cn/nix-channels/store/w5v7c0ikm6mzf17f3zyjvf0cn8gcammm.narinfo 
+pub struct Narinfo {
+	pub mut:
+	store_path string
+	relative_url string // nar/wthash.nar.xz
+	compression string // xz, ...
+	file_hash string // hashtype:hashvalue
+	file_size i64
+	nar_hash string  // hashtype:hashvalue
+	nar_size i64
+	references []string // depeneds store path, no /nix/store prefix
+	deriver string // .drv?
+	signt string
+}
+
+
+/////
 pub fn runcmdv(cmd string, wkdir ... string) []string {
 	dir := firstofv(wkdir)
 	scc := runcmd(cmd, dir, true)
@@ -96,6 +169,7 @@ pub fn cmdline_split(cmd string) []string{
 	return cmd.split(" ")
 }
 pub fn cmdfile_reform(file string) string {
+	// todo check abspath
 	if os.base(file) == file {
 		// line := os.system("which $file") // no capture
 		line := runcmd("/usr/bin/which $file", "", true)
@@ -126,9 +200,14 @@ struct Cmdarg {
 // todo temp repack directory
 // todo depends packages resolve
 // linked shared library resolve
+// todo remove share/man/info file
 // todo max candidate lines to show option
 // todo meta data file auto update
 // todo implement nix copy, drop nix depened
+// todo rootless mode, specifiy package install base
+// todo specifiy nix channel version
+// todo avoid nix copy depeneds
+// todo order candidate store path, prefix, suffix...
 // prerequires: sudo!!! tar brotli grep install_name_tool
 // sometimes need select package.
 // sometimes need input root password
@@ -140,9 +219,9 @@ fn main() {
 	vcp.info(nomats.str())
 	if cfg.show_help || true {
 		doc := flag.to_doc[Cmdarg]()!
-		println(doc)
+		// println(doc)
 	}
-	dump(cfg)
+	// dump(cfg)
 
 	println('Hello World! ${os.args}, ${nomats}')
 	// if true { return }
@@ -150,7 +229,9 @@ fn main() {
 	args := nomats
 	pkgname := args[0]
 	// store_path_file := "store-path-head200" // about 15K
-	store_path_file := "store-path" // about 11M
+	// store_path_file := "store-path" // about 11M, stable 24.05
+	store_path_file := "store-paths" // about 24M, unstable
+	mut nix := Nixbase{hosturl:hosturl}
 	
 	vcp.info("reading store-path maybe need secs...")
 	rcvals := get_match_store_paths(store_path_file, pkgname)
@@ -159,6 +240,8 @@ fn main() {
 	if rcvals.len == 0 {
 		exit(-1)
 	}
+	
+	// todo filter by binarch
 
 	maxoptno := 30
 	for i, line in rcvals {
@@ -185,17 +268,17 @@ fn main() {
 	// vcp.info(ino)
 	if ino < 0 { vcp.info("invalid select no", ino, ipt);  exit(-1) }
 
-		if ino >= 0 && ino < rcvals.len {
+	if ino >= 0 && ino < rcvals.len {
 			// parse line
 			pkgline := rcvals[ino]
 			hv, pkg, ver := parse_nixstore_line(pkgline)
 			vcp.info(hv, pkg, ver)
 
-			nix := Nixbase{hosturl}
+
 			stpurl := nix.storeurl() + pkgline
 			vcp.info("store path full", stpurl)
 
-			runcmd("nix copy ${pkgline} --to pkgs/ --impure --no-use-registries --no-update-lock-file --no-write-lock-file --no-recursive --refresh --repair -v", "", false)
+			runcmd("nix copy ${pkgline} --to pkgs/ --impure --no-use-registries --no-update-lock-file --no-write-lock-file --no-recursive --refresh --repair -v --debug", "", false)
 			pkgdir := "pkgs/${pkgline}"
 			dotsrcinfo := pkgdir + "/.PKGINFO"
 			vcp.info("saved", os.exists(pkgdir), pkgdir)
@@ -226,6 +309,9 @@ fn main() {
 
 			// resolve dlpath
 			replace_sharelib_ldpaths("pkgs/usr/local")
+			// some clean share/man/info
+			runcmd("sudo rm -rf pkgs/usr/local/share/man", "", false)
+			runcmd("sudo rm -rf pkgs/usr/local/share/info", "", false)
 
 			runcmd("fakeroot -- tar zcfp ${mydir}/${tmpgz} usr/ .PKGINFO", os.real_path( "pkgs/"), false)
 			runcmd("tar tf ${tmpgz}", "", false)
@@ -233,13 +319,51 @@ fn main() {
 			runcmd("mv ${tmpgz} ${pkg}-${ver}.darwin.amd64.pkg.tar.gz", "", false)
 
 			vcp.info("cleanup pkgs/usr/local/ ...", "", false)
-			runcmd("rm -rf pkgs/usr/local", "", false)
+			// runcmd("rm -rf pkgs/usr/local", "", false)
 			runcmd("sudo rm -rf pkgs/usr/local", "", false)
-			runcmd("rm -rf pkgs/nix/store/", "", false)
+			// runcmd("rm -rf pkgs/nix/store/", "", false)
 			runcmd("sudo rm -rf pkgs/nix/store/", "", false)
 			runcmd("sudo rm -rf pkgs/nix/var/", "", false)
 		}
 	// demo()
+}
+
+pub struct Repacker {
+	pub mut:
+	nb &Nixbase = vnil
+	recursive bool
+	store_path string // original
+	narval Narinfo
+
+	depends []&Repacker
+	deriver &Repacker // must & or invalid recursive struct error
+}
+
+pub fn (mut me Repacker) detect_binarch() {
+
+}
+
+pub fn (mut me Repacker) dl_store_path() {
+
+}
+
+pub fn (mut me Repacker) packit() {
+
+}
+
+fn get_cmds_full_paths(cmds ... string) map[string]string{
+	return map[string]string{}
+}
+
+// /nix/store/w5v7c0ikm6mzf17f3zyjvf0cn8gcammm-xxx =>
+// w5v7c0ikm6mzf17f3zyjvf0cn8gcammm.narinfo
+// not need
+fn store_path_to_narpath(stpath string) string {
+	return stpath
+}
+
+fn cleanup_temps() {
+
 }
 
 fn addassignop<T>(v T, p voidptr) T {
@@ -257,7 +381,7 @@ fn replace_sharelib_ldpaths(dir string) {
 		}
 		if os.is_dir(f) || os.is_link(f) { return }
 		if !check_binarch(f) { addassignop(1, ctx)
-			// vcp.info(check_binarch(f), ctx, f)
+			vcp.info(check_binarch(f), ctx, f)
 		}
 		replace_sharelib_ldpath(f)
 	})
@@ -294,7 +418,7 @@ fn check_binarch(file string) bool {
 	lines := runcmdv("file ${file}")
 	filety := firstofv(lines)
 	// vcp.info(filety,  filety.contains("executable"), filety.contains("x86_64") )
-	if filety.contains("executable") && !filety.contains("x86_64") {
+	if filety.contains("Mach-O") && filety.contains("executable") && !filety.contains("x86_64") {
 		return false
 	}
 	return true
@@ -391,7 +515,7 @@ fn get_match_store_paths_grep(store_path_file string, kw string) []string{
 }
 
 fn demo() {
-	nix := Nixbase{hosturl}
+	nix := Nixbase{hosturl:hosturl}
 
 	mut ch := curlv.new()
 	ch.url(nix.pkgurl())
